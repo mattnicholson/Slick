f0c40d7ac33f2da0b43785728e8ef86ed6f0fba7
Matt Nicholson <mjn456@gmail.com>
2016-10-28 11:08:10 +0100

    Example index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+
+/*
+	Include slick.php for fast caching
+	+++++++++++++++++++++++++++++++++++++++++++++++
+*/
+
+	include 'slick.php';
+
+/*
+	
+	End slick.php, carry on with application code...
+	++++++++++++++++++++++++++++++++++++++++++++++++
+		
+*/
+
+
+/*
+
+  Paste the contents of your original index.php below here:
+  
+*/
+
+// Your original index.php code....
